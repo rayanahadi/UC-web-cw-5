46730fb533c8ae9029b4646fe44a45b60900c3fb
rayanhadi <rayanalkhaldi570@gmail.com>
2023-09-04 22:29:21 +0300

    done for today

---
+++ script.js
@@ -0,0 +1,27 @@
+ .{alert  مرحبا بك في موقعي! انا ريان وسوف احسب درجتك 
+    
+
+    let grade = prompt ("ادخل درحتك  ")
+    console.log(grade)
+
+    if  (grade>=90 && grade<= 100   ) { console.log ("لقد حصلت على امتياز ")   }
+    else if (grade>=80 &&  grade<=89) { console.log (" لقد حصلت على جيد جدا ")}
+    if( grade>=70 && grade<=79) { console.log(  "لقد حصلت على جيد" )}
+    if( grade>=60 && grade<=69){ console.log(" لقد حصلت على ضعيف")              }
+    if( grade>=50 && grade<=59) { console.log(" لقد حصلت على ضعيف")}
+    if( grade>=49 ){ console.log("راسب")}
+
+
+
+
+
+
+
+
+
+
+} 
+
+
+
+.{ prompt ادخل درجتك}
